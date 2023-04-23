318129bc50e48a89aa817bda24f354f967a2194e
Gabriel Puebla <gabrielpuebla16@gmail.com>
2023-04-22 22:57:04 -0300

    solving fist prompt to Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string latterGrade;
+
+        Console.WriteLine("Letter Grade Calculator");
+        Console.Write("Please, enter your grade percentage:");
+        String gradePorcentage = Console.ReadLine();
+        float grade = float.Parse(gradePorcentage);
+
+        if (grade>=90)
+        {
+            latterGrade = "A";
+        }
+        else if (grade>=80)
+        {
+            latterGrade = "B";
+        }
+        else if (grade>=70)
+        {
+            latterGrade = "C";
+        }
+        else if (grade>=60)
+        {
+            latterGrade = "E";
+        }
+        else
+        {
+            latterGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {latterGrade}");
     }
 }
