c0c979da9b85a0a34dccd97ca3805cbb43a56c39
Gabriel Puebla <gabrielpuebla16@gmail.com>
2023-04-26 22:42:30 -0300

    Prep3 prompt 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("-------------------------- Find the magic number --------------------------");
+        string result= "";
+
+        Console.Write("What is the magic number? ");
+        int magic_number = int.Parse(Console.ReadLine());
+        Console.Write("What is your guess? ");
+        int userGuess =  int.Parse(Console.ReadLine());
+
+            if(magic_number < userGuess)
+            {
+                result = "Lower";
+            }
+            else if(magic_number > userGuess)
+            {
+                result = "Higher";
+            }
+            else if(magic_number == userGuess)
+            {
+                result = "You guessed it!";
+            }
+
+        Console.WriteLine($"{result}");
+    
     }
 }
