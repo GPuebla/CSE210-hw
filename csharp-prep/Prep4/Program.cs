5c13591a5d0a6d05ccef52d8a338216b670f1d49
Gabriel Puebla <gabrielpuebla16@gmail.com>
2023-04-27 00:32:47 -0300

    sum list - promp1

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("------------------------ Lists ------------------------");
+
+        List<int> numbers = new List<int>();
+        int numSum = 0;
+        float numAvg = 0;
+        int largestNum = 0;
+
+        int userNum = 0;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            userNum = int.Parse(Console.ReadLine());
+            numbers.Add(userNum);
+        } while (userNum != 0);
+
+        foreach (var num in numbers)
+        {
+          numSum += num; 
+        }
+
+        Console.WriteLine($"The sum is:{numSum}");
+        
     }
 }
