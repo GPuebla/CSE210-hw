daa9dac526f47b6d05cbc6ef739950e0241e4df3
Gabriel Puebla <gabrielpuebla16@gmail.com>
2023-05-27 12:19:29 -0300

    adding class verse

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -6,6 +6,7 @@ class Program
     {
 
         List<Word> words = new List<Word>();
+        List<Word> words2 = new List<Word>();
 
         Word w1 = new Word("Hello");
         Word w2 = new Word("World!");
@@ -13,23 +14,46 @@ class Program
         Word w4 = new Word("are");
         Word w5 = new Word("you?");
 
-        words.Add(w1);
-        words.Add(w2);
-        words.Add(w3);
-        words.Add(w4);
-        words.Add(w5);
+        Word w6 = new Word("This");
+        Word w7 = new Word("is");
+        Word w8 = new Word("a");
+        Word w9 = new Word("verse");
+        Word w10 = new Word("example");
 
-        foreach (var w in words)
-        {
-            Console.Write($"{w.GetWord()} ");
-        }
 
-        foreach (var w in words)
+        words2.Add(w1);
+        words2.Add(w2);
+        words2.Add(w3);
+        words2.Add(w4);
+        words2.Add(w5);
+
+        words2.Add(w6);
+        words2.Add(w7);
+        words2.Add(w8);
+        words2.Add(w9);
+        words2.Add(w10);
+
+        Verse v1 = new Verse(1,"example1",words2);
+
+        do
         {
-            w.HideWord();
-            Console.Write($"{w.GetWord()} ");
-        }
-        
+            Console.WriteLine($"{v1.WriteVerse()}, {v1.GetTotalWordsHide()},{v1.GetTotalWordsVisible()}-- {v1.IsAllHidden}");
+            Console.ReadLine();
+            Console.Clear();
+        } while (!v1.IsAllHidden);
+
+        // Console.WriteLine($"{v1.WriteVerse()}, {v1.GetTotalWordsHide()},{v1.GetTotalWordsVisible()} -- {v1.IsAllHidden}");
+        // Console.WriteLine($"{v1.WriteVerse()}, {v1.GetTotalWordsHide()},{v1.GetTotalWordsVisible()} -- {v1.IsAllHidden}");
+        // Console.WriteLine($"{v1.WriteVerse()}, {v1.GetTotalWordsHide()},{v1.GetTotalWordsVisible()} -- {v1.IsAllHidden}");
+        // Console.WriteLine($"{v1.WriteVerse()}, {v1.GetTotalWordsHide()},{v1.GetTotalWordsVisible()} -- {v1.IsAllHidden}");
+        // Console.WriteLine($"{v1.WriteVerse()}, {v1.GetTotalWordsHide()},{v1.GetTotalWordsVisible()} -- {v1.IsAllHidden}");
+    
+
+
+        // Console.WriteLine(v1.WriteVerse()); 
+        // Console.WriteLine(v1.WriteVerse()); 
+        // Console.WriteLine(v1.WriteVerse()); 
+        // Console.WriteLine(v1.WriteVerse()); 
 
     }
 }
--- prove/Develop03/Verse.cs
+++ prove/Develop03/Verse.cs
@@ -5,15 +5,32 @@ public class Verse
     private int _numberVerse{get;}
     private String _bookName;
     private String _chapterName;
-    private bool _isAllHidden;
+    private String _sentence;
+    private bool _isAllHidden{get;set;}
+    private int _executionAccount = 0;
+    private int _totalWordsHide;
+    private int _totalWordsVisible;
+    private Random rdm = new Random();
 
     private List<Word> words = new List<Word>();
 
-    public Verse (int numVer, String bookName )
+    public Verse (int numVer, String bookName, Word singleWord )
     {
         this._numberVerse = numVer;
         this._bookName = bookName;
         checkedIfAllAreHidden();
+        CountWordsHideAndVisible();
+
+    }
+
+    public Verse (int numVer, String bookName, List<Word> listOfWords )
+    {
+        this._numberVerse = numVer;
+        this._bookName = bookName;
+        checkedIfAllAreHidden();
+        CountWordsHideAndVisible();
+
+        this.words.AddRange(listOfWords);
     }
 
     private void checkedIfAllAreHidden()
@@ -26,6 +43,100 @@ public class Verse
         }
     }
 
+    public List<Word> GetWords()
+    {
+        return this.words;
+    }
 
+    public String WriteVerse()
+    {
+        int lap = 0;
+        _sentence = "";
+
+        if(_executionAccount == 0)
+        {
+            foreach (var w in words)
+            {
+                _sentence += $" {w.GetWord()}";
+            }
+            _executionAccount++;
+        }
+        else if(_totalWordsVisible>3)
+        {
+            while(lap<3)
+            {
+                int index = rdm.Next(words.Count());
+
+                if(!words[index].getIsHide())
+                {
+                  lap ++;
+                  words[index].HideWord();
+                }
+            }
+
+            foreach (var w in words)
+            {
+                _sentence += $" {w.GetWord()}";
+            } 
+
+        }
+        else{
+
+            foreach (var i in words)
+            {
+                 if(!i.getIsHide())
+                {
+                  i.HideWord();
+                }
+            }
+
+            foreach (var w in words)
+            {
+                _sentence += $" {w.GetWord()}";
+            } 
+        }
+        CountWordsHideAndVisible();
+
+        return _sentence;
+    }
+
+    private void CountWordsHideAndVisible()
+    {
+        _totalWordsHide = 0;
+        _totalWordsVisible = 0;
+        foreach (var i in words)
+        {
+            if(i.getIsHide())
+            {
+                _totalWordsHide++;
+            }
+            else
+            {
+                _totalWordsVisible++;
+            }
+        }
+
+        if (_totalWordsVisible == 0)
+        {
+            _isAllHidden = true;
+        }else
+        {
+            _isAllHidden = false;
+        }
+    }
+
+    public int GetTotalWordsHide()
+    {
+        return this._totalWordsHide;
+    }
+    public int GetTotalWordsVisible()
+    {
+        return this._totalWordsVisible;
+    }
+
+    public bool IsAllHidden
+    {
+        get{ return this._isAllHidden;}
+    }
 
 }
