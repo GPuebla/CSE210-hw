f85a50d980f4daa0dbfb5db151b6ddcb7cd08b44
Gabriel Puebla <gabrielpuebla16@gmail.com>
2023-06-11 13:00:45 -0300

    UML diagram update

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop04 World!");
+        
+    for (int i = 0; i < 30; i++)
+    {
+        Console.Write("+");
+
+        Thread.Sleep(500);
+
+        Console.Write("\b \b"); // Erase the + character
+        Console.Write("-"); // Replace it with the - character
+    }
+    
+
     }
 }
