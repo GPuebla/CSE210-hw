048ac2c86ddd58d590715e3cd53a5828b86f65ef
Gabriel Puebla <gabrielpuebla16@gmail.com>
2023-06-24 21:14:41 -0300

    updating dataset

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -5,44 +5,60 @@ class Program
     static void Main(string[] args)
     {
         int totalScore = 0;
+        DataSet dataset = new DataSet();
+
         SimpleGoal sGoal1 = new SimpleGoal("Simple Goal Example 1","This goal, is just an example",30);
         EternalGoal eGoal1 = new EternalGoal("Eternal Goal Example 1","This goal, is just an example",15);
         CheckListGoal cGoal1 = new CheckListGoal("CheckList Goal Example 1","This goal, is just an example",50,500,5);
 
         List<Goal> goals = new List<Goal>();
-        goals.Add(sGoal1);
-        goals.Add(eGoal1);
-        goals.Add(cGoal1);
+        goals.Add(new SimpleGoal("Simple Goal Example 2","This goal, is just an example 2",50));
+        // goals.Add(eGoal1);
+        // goals.Add(cGoal1);
 
-        // sGoal1.IncreaseScore();
-        // sGoal1.IncreaseScore();
-        // sGoal1.IncreaseScore();
+        Console.WriteLine($"Total de Goals: {dataset.GetGoalList().Count()}");
 
-        for (int i = 0; i < 5; i++)
+        dataset.ReadData("my_goal");
+
+        goals.AddRange(dataset.GetGoalList());
+
+        
+        // dataset.AddGoal(sGoal1);
+        // dataset.AddGoal(eGoal1);
+        // dataset.AddGoal(cGoal1);
+
+        Console.WriteLine($"Total de Goals: {dataset.GetGoalList().Count()}");
+
+        foreach (var goal in dataset.GetGoalList())
         {
-            foreach (var goal in goals)
-            {
-                DisplayGoals(goal);
-            } 
-        }
+            Console.WriteLine($"{goal.ConcatenateAttribute()}");
+        } 
 
-        foreach (var goal in goals)
-        {
-            totalScore += goal.GetScore();
-        }
+        Console.WriteLine(dataset.GetScore());
 
-        Console.WriteLine($"TOTAL SCORE: {totalScore}");
+        dataset.AddGoal(new SimpleGoal("Simple Goal Example 2","This goal, is just an example 2",50));
 
+        dataset.SaveData();
 
-        // Console.WriteLine(sGoal1.ConcatenateAttribute());
-        // Console.WriteLine($"Score: {sGoal1.GetScore()}");
 
     }
+//         foreach (var goal in dataset.GetGoalList())
+//         {
+//             totalScore += goal.GetScore();
+//             dataset.SetScore(totalScore);
+//             DisplayGoals(goal);
+//         }
 
-    public static void DisplayGoals(Goal g)
-    {
-        g.IncreaseScore();
-        Console.WriteLine($"{g.ConcatenateAttribute()} // Score: {g.GetScore()}");
-        Console.WriteLine("----------------------------------------------------------------------------\n");
-    }
+//         Console.WriteLine($"TOTAL SCORE: {totalScore}");
+
+//     //    dataset.SaveData();
+
+//     }
+
+//     public static void DisplayGoals(Goal g)
+//     {
+//         g.IncreaseScore();
+//         Console.WriteLine($"{g.ConcatenateAttribute()} // Score: {g.GetScore()}");
+//         Console.WriteLine("----------------------------------------------------------------------------\n");
+//     }
 }
